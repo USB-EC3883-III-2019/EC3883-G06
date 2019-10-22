00f73fc0642b735e30bd1b1882a827d47c54fd34
Jesus Colmenares <14-11384@usb.ve>
2019-10-22 15:57:01 -0400

    Add logger

--- software/solindar_lib/__init__.py
+++ software/solindar_lib/__init__.py
@@ -2,4 +2,4 @@ from .ComDriver import ComDriver
 from .GUI import *
 from .TestCom import testCom
 from .process_data import *
-
+from .logger import *
---
+++ software/solindar_lib/logger.py
@@ -0,0 +1,18 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+class logger:
+    #Setting up logger
+    formatter = logging.Formatter('%(asctime)s: %(message)s') #Log format
+    max_kbytes=2*1024
+
+    def __init__(self):
+            path = 'logs'
+            self.logger_info = logging.getLogger(__name__ + '_INFO') #logger for general debugging purposes, from debug to warning levels
+            self.logger_info.setLevel(logging.DEBUG)
+            file_handler_info =  RotatingFileHandler(path + '/SOLIDAR_INFO.log', mode='a', maxBytes=self.max_kbytes*1024, backupCount=2, encoding=None, delay=0)
+            file_handler_info.setFormatter(self.formatter)
+            self.logger_info.addHandler(file_handler_info)
+
+    def info(self,log):
+        return self.logger_info.info(log)
