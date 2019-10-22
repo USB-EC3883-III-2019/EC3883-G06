3ce173f29e440f0c9a876b8de472846415fa359c
Christian Dos Santos <cdsantos02@hotmail.com>
2019-10-22 15:57:51 -0400

    GuiLink

--- software/main.py
+++ software/main.py
@@ -6,11 +6,13 @@ from solindar_lib import ComDriver, SolindarGUI, TestCom
 import qdarkstyle
 
 #Implementation
-ts=100 #sampling time in ms
-refresh_time=100 #time for updating graph in ms
+ts=1000 #sampling time in ms
+refresh_time=1000 #time for updating graph in ms
 len_fifo = 63
 n_block = 10
-con = TestCom.testCom(len_fifo,n_block)
+Port = "COM3"
+# con = TestCom.testCom(len_fifo,n_block)
+con = ComDriver(Port,len_fifo,n_block)
 
 #The following lines open the gui app
 if __name__ == "__main__":
