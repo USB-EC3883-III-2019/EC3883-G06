4236f04020a06671d567b65855b2a3e5e1193f98
Jesus Colmenares <14-11384@usb.ve>
2019-10-28 12:10:04 -0400

    Set fusion var

--- software/solindar_lib/process_fusion.py
+++ software/solindar_lib/process_fusion.py
@@ -4,8 +4,8 @@ import numpy as np
 def process_fusion(sonar,lidar):
 
     #Fusion
-    sonvarn2 = (1.47*(10**-3))**-2
-    lidvarn2 = (4.89*(10**-5))**-2
+    sonvarn2 = 0.356860852
+    lidvarn2 = 1.717397544
     sonarMaxRange = 300
     fusvar2 = ((sonvarn2) + (lidvarn2))**(-1)
 
