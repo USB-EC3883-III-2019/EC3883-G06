bfb8ea091045a35fb88ff29c3ff20462af79b180
Christian Dos Santos <cdsantos02@hotmail.com>
2019-10-28 12:23:23 -0400

    VarUpdate

--- software/solindar_lib/process_fusion.py
+++ software/solindar_lib/process_fusion.py
@@ -7,10 +7,10 @@ def process_fusion(sonar,lidar):
     sonvarn2 = 0.356860852
     lidvarn2 = 1.717397544
     sonarMaxRange = 300
-    fusvar2 = ((sonvarn2) + (lidvarn2))**(-1)
+    fusvar2 = ((sonvarn2)**(-1) + (lidvarn2)**(-1))**(-1)
 
 
-    fusion = fusvar2*(sonvarn2*sonar + lidvarn2*lidar)
+    fusion = fusvar2*((sonvarn2**-1)*sonar + (lidvarn2**-1)*lidar)
     fusion[lidar >= 80] = sonar[lidar >= 80]
 
     return fusion
